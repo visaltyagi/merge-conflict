b41192767a44e08e15c0b1fa0464eb33ac116040
Ubuntu <ubuntu@ip-172-31-14-86.us-east-2.compute.internal>
2024-06-03 07:27:54 +0000

    2

--- main.c
+++ main.c
@@ -5,24 +5,19 @@ Initial Function()
   //Sample Code
 }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 Initial Security()
 {
  //Sample Code
-=======
+
 feature1()
 {
   //Sample Code for Feature 1
->>>>>>> feature1
 }
 
-=======
 feaature2()
 {
   //Sample Code for Feature 2
 }
->>>>>>> feature2
 
 
 }
